fb2bb0c0ab99138a2c9281032b23af91d36e95b3
Ashley <ashleyponce01@hotmail.com>
2021-04-18 12:17:51 -0700

    First commit

---
+++ Kingfisherr/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //TEMPORARY, just to get the player moving
+    //probably should replace with the movement learned in class
+
+    public CharacterController controller;
+    private Vector3 direction;
+    public float speed = 8;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+        direction.x = hInput * speed;
+        direction.y = vInput * speed;
+
+        controller.Move(direction * Time.deltaTime);
+    }
+}
---
+++ Kingfisherr/Assets/UIMgr.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMgr : MonoBehaviour
+{
+    public static UIMgr inst;
+    private void Awake()
+    {
+        inst = this;
+    }
+
+    public int air; //how much air the player has
+    public int bubbleCount; //max amount of air the player can have
+
+    public Image[] bubbles; //stores the bubbles in an array
+
+    //might not need these
+    public Sprite fullBubble;
+    public Sprite emptyBubble;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (air > bubbleCount) //make sure amount of air doesn't exceed amount of bubbles
+            air = bubbleCount;
+
+        for (int i = 0; i < bubbles.Length; i++)
+        {
+            if (i < air)
+                bubbles[i].sprite = fullBubble;
+            else
+                bubbles[i].sprite = emptyBubble;
+
+            if (i < bubbleCount)
+                bubbles[i].enabled = true;
+            else
+                bubbles[i].enabled = false;
+        }
+    }
+}
