638a9859d584289757f3928dd0e4f0f5d4bf5ae3
Ashley <ashleyponce01@hotmail.com>
2021-04-25 14:03:31 -0700

    updated scenes and meta files

--- Kingfisherr/Assets/Physics.cs
+++ Kingfisherr/Assets/Physics.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Physics : MonoBehaviour
+public class BasicPhysics : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
